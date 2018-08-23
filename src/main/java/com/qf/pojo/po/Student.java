8646c27d19ebfe54be9f6f1dc7306ccd243b47ae
wurim <747831582@qq.com>
2018-08-23 20:08:18 +0800

    添加了一个实体类

---
+++ src/main/java/com/qf/pojo/po/Student.java
@@ -0,0 +1,25 @@
+package com.qf.pojo.po;
+
+/**
+ * Created by wurim on 2018/8/23.
+ */
+public class Student {
+    private int id;
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
